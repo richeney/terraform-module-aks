c18a5cdcfb6d9a95ee098b0f4bd66580f86879b8
Richard Cheney <richeney@microsoft.com>
2018-11-02 17:54:50 +0000

    Removed user and password

--- provider.tf
+++ provider.tf
@@ -9,8 +9,6 @@ provider "random" {
 provider "kubernetes" {
     version                 = "~> 1.3"
     host                    = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
-    # username                = "${azurerm_kubernetes_cluster.aks.kube_config.0.username}"
-    # password                = "${azurerm_kubernetes_cluster.aks.kube_config.0.password}"
     client_certificate      = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)}"
     client_key              = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)}"
     cluster_ca_certificate  = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)}"
