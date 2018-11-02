ffb2df096de332c8af282c3f2f31a463e4f3b580
Richard Cheney <Richard.Cheney@microsoft.com>
2018-11-02 15:09:23 +0000

    Added cluster name

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config_raw}"
+output "cluster" {
+    value = "aks-${random_string.aks.result}"
 }
 
 output "host" {
   value = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
+}
+
+output "kube_config" {
+  value = "${azurerm_kubernetes_cluster.aks.kube_config_raw}"
 }
